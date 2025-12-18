ff4c272aadc44b583dc0953f6f76cc46de9601d8
Micah Avrill Bobis <micahavrill14@gmail.com>
2025-12-18 13:12:18 +0800

    Update ProjectCard.jsx

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,9 +2,14 @@ import React from "react";
 
 const ProjectCard = React.memo(({ title, desc, tech, github, live }) => (
   <div className="bg-beige rounded-xl shadow-md p-5 hover:shadow-lg hover:scale-[1.03] transition-transform duration-300 border border-graySoft">
-    <div className="h-32 bg-gray-200 mb-4 flex items-center justify-center rounded-xl text-gray-500">
-      Screenshot
-    </div>
+    <div className="h-32 mb-4 flex items-center justify-center rounded-xl overflow-hidden bg-gray-200 text-gray-500">
+  {img ? (
+    <img src={img} alt={title} className="w-full h-full object-cover" />
+  ) : (
+    "Screenshot"
+  )}
+</div>
+
     <h3 className="font-bold text-lg text-gray-800">{title}</h3>
     <p className="text-sm text-gray-700 mb-2">{desc}</p>
     <p className="text-xs text-gray-500 mb-4">{tech}</p>
